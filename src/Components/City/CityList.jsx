e700ba2287dc3669259cb47872a8762d2c471c9a
Karan Meghe <karanmeghe87@gmail.com>
2024-10-02 14:20:19 +0530

    add - adding cityItem + Message Component will help user if he gets empty data

--- src/Components/City/CityList.jsx
+++ src/Components/City/CityList.jsx
@@ -1,11 +1,14 @@
 import styles from './City.module.css'
 import { useContext } from 'react'
 import CityContext from '../../Context /CityContext'
-import { CityItem, Spinner } from '../index';
+import { CityItem, Message, Spinner } from '../index';
 const CityList = () => {
     const { cities, isLoading } = useContext(CityContext);
     if (isLoading) {
         return <Spinner />
+    }
+    if (!cities.length) {
+        return <Message message="Add your first city by clicking on the map" />
     }
     return (
         <ul style={styles.cityList}>
