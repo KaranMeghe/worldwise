ebaf8e94aaff4a07d955e452f628afe25d8dc013
Karan Meghe <karanmeghe87@gmail.com>
2024-10-03 22:18:32 +0530

    add - interaction with form 1) when we change city in list location change on map , 2) when we click anywhere in map we will get lat and lng value according to location in url

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,21 +1,39 @@
 import { useSearchParams, useNavigate } from 'react-router-dom'
 import styles from './Map.module.css'
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvent } from 'react-leaflet'
 import CityContext from '../../Context /CityContext';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+
+function ChangeCenter({ position }) {
+    const map = useMap();
+    map.setView(position);
+    return null;
+}
+
+function DetactClick() {
+    const navigate = useNavigate();
+    useMapEvent({ click: (e) => navigate(`form?Lat=${e.latlng.lat}&lng=${e.latlng.lng}`) })
+}
 
 const Map = () => {
-    const navigate = useNavigate();
-    const [searchParams, setSearchParams] = useSearchParams()
-    const [mapPosition, setMapPosition] = useState([28.6139, 77.2088])
-    console.log(searchParams)
-    const lat = searchParams.get("lat")
-    const lng = searchParams.get("lng")
-    const { cities } = useContext(CityContext)
+
+    const [searchParams] = useSearchParams();
+    const [mapPosition, setMapPosition] = useState([28.6139, 77.2088]);
+
+    const mapLat = searchParams.get("lat");
+    const mapLng = searchParams.get("lng");
+
+    const { cities } = useContext(CityContext);
+
+    useEffect(() => {
+        if (mapLat && mapLng) {
+            return setMapPosition([mapLat, mapLng])
+        };
+    }, [mapLat, mapLng])
 
     return (
         <div className={styles.mapContainer} >
-            <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={true}>
+            <MapContainer className={styles.map} center={mapPosition} zoom={8} scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -27,9 +45,14 @@ const Map = () => {
                         </Popup>
                     </Marker>
                 })}
+                <ChangeCenter position={mapPosition} />
+                <DetactClick />
             </MapContainer>
         </div>
     )
 }
+
+
+
 
 export default Map
